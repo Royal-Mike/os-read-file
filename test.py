e10a04e041eb444ca958e293b7f696e50899e56f
RoyalMike <bibo200311@gmail.com>
2023-03-14 01:16:55 +0700

    more testing

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-with open (r"\\.\PHYSICALDRIVE0","rb") as fr:
-    data = fr.read(512)
-    print(data)
+# PHYSICALDRIVE0 = Disk
+# C:, D: = Partition
+# with open (r"\\.\C:", "rb") as fr:
+#     count = 0
+#     byte = fr.read(1)
+#     while count < 512:
+#         print(byte.hex(), end=' ')
+#         byte = fr.read(1)
+#         count += 1
+
+with open (r"\\.\D:", "rb") as fr:
+    fr.read(3)
+    print(fr.read(5).decode("ascii"))
