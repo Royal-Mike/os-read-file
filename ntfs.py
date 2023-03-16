b8177918a167f5a1da5beb5beef957d40710e187
RoyalMike <bibo200311@gmail.com>
2023-03-16 13:39:14 +0700

    read name and size

--- ntfs.py
+++ ntfs.py
@@ -15,7 +15,7 @@ def twos_comp(val, bits):
         val = val - (1 << bits)
     return val
 
-with open (r"\\.\D:", "rb") as fp:
+with open(r"\\.\C:", "rb") as fp:
     fp.read(3)
     type = fp.read(4).decode("ascii")
     if (type == "NTFS"):
@@ -54,48 +54,64 @@ with open (r"\\.\D:", "rb") as fp:
 
         startMFTEntry = MFTstartByte
         i = 1
-        while i < 6:
+        while i < 20:
             print('File ' + str(i))
 
             nextMFTEntry = startMFTEntry + bytesPerMFTEntry
 
+            fp.seek(startMFTEntry, 0)
+            fp.read(1)
+
             fp.seek(startMFTEntry + 0x14, 0)
             offsetFirstAttribute = int.from_bytes(fp.read(2), 'little')
 
+            fp.seek(startMFTEntry + 0x18, 0)
+            sizeMFTEntryUsed = int.from_bytes(fp.read(4), 'little')
+            print('Size: ' + str(sizeMFTEntryUsed) + 'B')
+
             startAttribute = startMFTEntry + offsetFirstAttribute
 
-            while True:
-                fp.seek(startAttribute, 0)
-                typeAttribute = hex(int.from_bytes(fp.read(4), 'little'))
+            if (sizeMFTEntryUsed > 0):
+                while True:
+                    fp.seek(startAttribute, 0)
+                    typeAttribute = hex(int.from_bytes(fp.read(4), 'little'))
 
-                if typeAttribute == "0xffffffff": break
+                    if typeAttribute == "0xffffffff": break
 
-                fp.seek(startAttribute + 0x10, 0)
-                sizeContent = int.from_bytes(fp.read(4), 'little')
+                    fp.seek(startAttribute + 0x10, 0)
+                    sizeContent = int.from_bytes(fp.read(4), 'little')
 
-                fp.seek(startAttribute + 0x14, 0)
-                offsetContent = int.from_bytes(fp.read(2), 'little')
+                    fp.seek(startAttribute + 0x14, 0)
+                    offsetContent = int.from_bytes(fp.read(2), 'little')
 
-                startContent = startAttribute + offsetContent
+                    startContent = startAttribute + offsetContent
 
-                # $STANDARD_INFORMATION
-                if (typeAttribute == "0x10"):
-                    fp.seek(startContent, 0)
-                    timeCreatedNS = int.from_bytes(fp.read(8), 'little')
-                    organizedTime = datetime(1601, 1, 1, 0, 0, 0) + timedelta(seconds = timeCreatedNS/1e7)
-                    dateCreated = (str(organizedTime.day) + "/" + str(organizedTime.month) + "/" + str(organizedTime.year))
-                    timeCreated = (str(organizedTime.hour) + ":" + str(organizedTime.minute) + ":" + str(organizedTime.second) + "." + str(organizedTime.microsecond))
-                    print(dateCreated)
-                    print(timeCreated)
+                    # $STANDARD_INFORMATION
+                    if (typeAttribute == "0x10"):
+                        fp.seek(startContent, 0)
+                        timeCreatedNS = int.from_bytes(fp.read(8), 'little')
+                        organizedTime = datetime(1601, 1, 1, 0, 0, 0) + timedelta(seconds = timeCreatedNS/1e7)
+                        dateCreated = (str(organizedTime.day) + "/" + str(organizedTime.month) + "/" + str(organizedTime.year))
+                        timeCreated = (str(organizedTime.hour) + ":" + str(organizedTime.minute) + ":" + str(organizedTime.second) + "." + str(organizedTime.microsecond))
+                        print('Date created: ' + str(dateCreated))
+                        print('Time created: ' + str(timeCreated))
 
-                # $FILE_NAME
-                elif (typeAttribute == "0x30"):
-                    print()
+                    # $FILE_NAME
+                    elif (typeAttribute == "0x30"):
+                        fp.seek(startContent + 0x38, 0)
+                        fileAttribute = int.from_bytes(fp.read(4), 'little')
+                        fp.seek(startContent + 0x40, 0)
+                        lengthFileName = int.from_bytes(fp.read(1), 'little')
+                        fp.seek(startContent + 0x42, 0)
+                        fileName = fp.read(lengthFileName * 2).replace(b'\x00', b'').decode('utf-8')
+                        print('Name: ' + fileName)
 
-                fp.seek(startAttribute + 0x04, 0)
-                sizeAttribute = int.from_bytes(fp.read(4), 'little')
+                    fp.seek(startAttribute + 0x04, 0)
+                    sizeAttribute = int.from_bytes(fp.read(4), 'little')
 
-                startAttribute += sizeAttribute
+                    startAttribute += sizeAttribute
+
+            print()
 
             startMFTEntry += bytesPerMFTEntry
             i += 1
