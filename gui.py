9660748895e96d035b54f917d1f31d0829bac167
RoyalMike <bibo200311@gmail.com>
2023-03-25 16:21:56 +0700

    gui trial

--- gui.py
+++ gui.py
@@ -14,43 +14,69 @@ menu_bar = tk.Menu(window)
 window.config(menu=menu_bar)
 
 # Create submenu
-partition_menu = tk.Menu(menu_bar)
+partition_menu = tk.Menu(menu_bar, tearoff='off')
 menu_bar.add_cascade(label='Partition', menu=partition_menu)
 
 # Create treeview
 tree = ttk.Treeview(window, height=20)
 tree.pack()
+tree.place(x=0)
 
 # Create labels
 name_label = tk.Label(window, text='Name:')
-name_label.place(x=20, y=320)
+name_label.place(x=220, y=0)
 
 attribute_label = tk.Label(window, text='Attribute:')
-attribute_label.place(x=20, y=350)
+attribute_label.place(x=220, y=30)
 
 date_label = tk.Label(window, text='Date Created:')
-date_label.place(x=20, y=380)
+date_label.place(x=220, y=60)
 
 time_label = tk.Label(window, text='Time Created:')
-time_label.place(x=20, y=410)
+time_label.place(x=220, y=90)
 
 size_label = tk.Label(window, text='Size:')
-size_label.place(x=20, y=440)
+size_label.place(x=220, y=120)
 
 # Create entry boxes
-name_entry = tk.Entry(window)
-name_entry.place(x=100, y=320)
+name_entry = tk.Label(window)
+name_entry.place(x=300, y=0)
 
-attribute_entry = tk.Entry(window)
-attribute_entry.place(x=100, y=350)
+attribute_entry = tk.Label(window)
+attribute_entry.place(x=300, y=30)
 
-date_entry = tk.Entry(window)
-date_entry.place(x=100, y=380)
+date_entry = tk.Label(window)
+date_entry.place(x=300, y=60)
 
-time_entry = tk.Entry(window)
-time_entry.place(x=100, y=410)
+time_entry = tk.Label(window)
+time_entry.place(x=300, y=90)
 
-size_entry = tk.Entry(window)
-size_entry.place(x=100, y=440)
+size_entry = tk.Label(window)
+size_entry.place(x=300, y=120)
+
+def open_partition(partition):
+    tree.delete(*tree.get_children())
+    if partition == 'FAT32':
+        tree.insert('', 'end', 'testF', text='testFAT')
+    elif partition == 'NTFS':
+        tree.insert('', 'end', 'testN', text='testNTFS')
+
+def display_info(self):
+    # Get the selected item
+    item = tree.selection()[0]
+    print(item)
+    # Display the information
+    name_entry['text'] = "A"
+    attribute_entry['text'] = "B"
+    date_entry['text'] = "C"
+    time_entry['text'] = "D"
+    size_entry['text'] = "E"
+
+# Add commands to the menu
+partition_menu.add_command(label='FAT32', command=lambda:open_partition('FAT32'))
+partition_menu.add_command(label='NTFS', command=lambda:open_partition('NTFS'))
+
+# Bind the treeview to the display_info function
+tree.bind('<<TreeviewSelect>>', display_info)
 
 window.mainloop()
